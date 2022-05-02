3d7cd22a2defafee7101646b2d302482f1493179
donat2021 <salamon.donat2@gmail.com>
2022-05-02 13:01:58 +0300

    tests 4.7

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -212,3 +212,84 @@ describe('Most popular', () => {
         expect(result).toBe(finalCounter)
     })
 })
+describe('Most likes', () => {
+    const blogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+            __v: 0
+        }
+    ]
+    //ask heikki about 4.7
+    test('Most liked author', () => {
+        const result = listHelper.mostLikes(blogs)
+        const usedAuthor = []
+        let usedCounter=0
+        let sum = 0
+        let finalSum=0
+        for (i = 0; i < blogs.length; i++)
+        {
+            if (!usedAuthor.includes(blogs[i].author)) 
+            {
+                sum=0
+                for (j = 0; j < blogs.length; j++)
+                {
+                    if(blogs[j].author==blogs[i].author)
+                    {
+                        sum+=blogs[j].likes
+                    }
+                }
+                usedAuthor[usedCounter]=blogs[i].author
+                usedCounter++
+                if(sum>finalSum)
+                {
+                    finalSum=sum
+                }
+            }
+        }
+        expect(result).toBe(finalSum)
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -49,6 +49,34 @@ const mostBlogs=(blogs)=>
         }
         return finalCounter
 }
+const mostLikes=(blogs)=>
+{
+    const usedAuthor = []
+        let usedCounter=0
+        let sum = 0
+        let finalSum=0
+        for (i = 0; i < blogs.length; i++)
+        {
+            if (!usedAuthor.includes(blogs[i].author)) 
+            {
+                sum=0
+                for (j = 0; j < blogs.length; j++)
+                {
+                    if(blogs[j].author==blogs[i].author)
+                    {
+                        sum+=blogs[j].likes
+                    }
+                }
+                usedAuthor[usedCounter]=blogs[i].author
+                usedCounter++
+                if(sum>finalSum)
+                {
+                    finalSum=sum
+                }
+            }
+        }
+        return finalSum
+}
   module.exports = {
-    dummy,totalLikes,favoriteBlog,mostBlogs
+    dummy,totalLikes,favoriteBlog,mostBlogs,mostLikes
   }
